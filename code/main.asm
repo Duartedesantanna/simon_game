45dd577845afa482937bff8bbee4a9a011e689d3
duartedesantanna <duartedesantanna@gmail.com>
2020-02-06 11:33:17 -0300

    Implementação da rotina de inicialização

--- code/main.asm
+++ code/main.asm
@@ -1,36 +1,77 @@
-#include <p16f887.inc>
+#include <pi16f887.inc>
 list p=16f887
 
-	cblock 0x20
+	cblock	0x20		;nomendo endereco da mamória
 		led_cnt
 	endc
 
-	org		0x00	; reset vector
+	org 	0x00		;vetor de reset
 	goto 	Start
 	
-	org		0x04	;interrupt vector
+	org 	0x04		;vetor interrupção
 	retfie
 	
 Start:
-	;----- I/O config ------
-	bsf 	STATUS, RP0	; change to bank1
-	movlw 	B'11110000'
-	movwf 	TRISA		; config RA0-R3 as ouput
-						; and RA4-RA7 as input
-	bsf 	STATUS, RP1	; change to bank3
-	clrf	ANSEL		; configure all PORTA,
-						; pins as digital I/O
+	;---- I/O config ----
+	;tris 0 input
+	;tris 1 output
+	
+	bsf STATUS, RP0 	;seleciona do bank0 00 to bank1 01
+	movlw B'11110000'	;bit menos significativo à direita
+	movwf TRISA			;configura RA0-RA3 como output
+						;configura RA4-RA7 como input
+	bsf STATUS, RP1		;seleciona do bank1 01 to bank3 11
+	clrf ANSEL 			;configura PORTA como entrada digital
+	
 Main:
-	goto 	RotinaInicializacao
-	
+
+	call RotinaInicializacao
+	;bcf STATUS, RPO		;seleciona do bank3 11 to bank2 10
+	;bcf STATUS, RP1		;seleciona do bank2 10 to bank0 00
+
 RotinaInicializacao:
-	bcf 	STATUS, RP1
-	bcf 	STATUS, RP0 ; change to bank0
-	movlw 	0x0F			
-	movwf 	PORTA		; set pins RA0-RA3
-	call	Delay_1s	; call delay function
-	clrf	PORTA		; reset pins RA0-RA3		
 	
-						
+	bcf	 	STATUS, RPO		;seleciona do bank3 11 to bank2 10
+	bcf 	STATUS, RP1		;seleciona do bank2 10 to bank0 00
+	movlw 	0x0F			;movendo b"00001111"
+	movwf	PORTA			;ligando os LEDs
+							;set pinos RA0-RA3
+	call 	Delay_1s		;chama função Delay
+	clrf	led_cnt			;led_cnt = 0
 	
+LedContLoop:
+
+	clrf 	PORTA			;clear PORTA zerando pinos RA0-RA3
+	
+	movlw	.0
+	subwf	led_cnt, W		;subtrai w de led_cont
+	btfsc	STATUS, Z		;comparação led_cnt = 0?
+	bsf		PORTA,RA0		;sim
+							;não
+	movlw	.1
+	subwf	led_cnt, W		;subtrai w de led_cont
+	btfsc	STATUS, Z		;comparação led_cnt = 1?
+	bsf		PORTA,RA1		;sim
+							;não
+	movlw	.2
+	subwf	led_cnt, W		;subtrai w de led_cont
+	btfsc	STATUS, Z		;comparação led_cnt = 2?
+	bsf		PORTA,RA2		;sim
+							;não
+	movlw	.3
+	subwf	led_cnt, W		;subtrai w de led_cont
+	btfsc	STATUS, Z		;comparação led_cnt = 3?
+	bsf		PORTA,RA3		;sim
+							;não
+							
+	call	Delay_200ms		;chama função delay_200ms
+	
+	incf	led_cnt, F		;inclemente led_cont
+	
+	movlw	.4
+	subwf	led_cnt, W		;subtrai w de led_cont
+	btfss	STATUS, Z		;comparação led_cnt = 4?
+	goto	LedContLoop		;não
+	clrf	PORTA			;sim
+	return
 	
